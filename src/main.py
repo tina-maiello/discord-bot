06310fe9cc26a924002263a48c518d1aa71930cb
tina-maiello <tina.maiello@pm.me>
2025-04-27 18:02:15 -0400

    refactored main to be a bit more split up into components, not married to the current layout though.

--- src/main.py
+++ src/main.py
@@ -4,24 +4,32 @@ from dotenv import load_dotenv
 import discord
 import argparse
 from os import environ
+logger = LoggingFormatter.init_logger(__name__)
+
 
 # argument parsing
-parser = argparse.ArgumentParser(description="discord bot")
-parser.add_argument("-e","--emoji", help="takes in a custom unicode emoji to use as 'starboard' emoji",required=False)
-parser.add_argument("-sc","--starboard_channel", help="takes in a custom string to set as starboard channel",required=False)
-parser.add_argument("-rc","--reaction_count", help="takes in an integer number of reactions to put message into starboard_channel")
+def get_args():
+    parser = argparse.ArgumentParser(description="discord bot")
+    parser.add_argument("-e","--emoji", help="takes in a custom unicode emoji to use as 'starboard' emoji",required=False)
+    parser.add_argument("-sc","--starboard_channel", help="takes in a custom string to set as starboard channel",required=False)
+    parser.add_argument("-rc","--reaction_count", help="takes in an integer number of reactions to put message into starboard_channel")
+    return parser.parse_args()
 
-def main():
-    logger = LoggingFormatter.init_logger(__name__)
-    token_path = "data/.env"
-    logger.debug(f'setting environment variables from: {token_path}')
-    load_dotenv(token_path)
+
+def load_dotenv_environ_variables(path):
+    logger.debug(f'setting environment variables from: {path}')
+    load_dotenv(path)
+
+
+def get_intents_for_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
-    token_name = "BOT_SECRET"
-    logger.info(f'getting token from environment variable {token_name}')
-    bot_secret = environ.get(token_name)
-    args = parser.parse_args()
+    return intents
+
+
+def init_bot(token_name):
+    intents = get_intents_for_discord_bot()
+    args = get_args()
     if args.reaction_count:
         if args.emoji and args.channel:
             client = StarboardClient.StarboardClient(intents=intents,emoji=args.emoji,starboard_channel=args.channel, reaction_count=args.reaction_count)
@@ -29,7 +37,13 @@ def main():
             client = StarboardClient.StarboardClient(intents=intents, reaction_count=args.reaction_count)
     else:
         client = StarboardClient.StarboardClient(intents=intents)
-    client.run(bot_secret)
+    logger.info(f'getting token from environment variable {token_name}')
+    client.run(environ.get(token_name))
+    
+
+def main():
+    load_dotenv_environ_variables("data/.env")
+    init_bot("BOT_SECRET")
 
 
 if __name__ == "__main__":
